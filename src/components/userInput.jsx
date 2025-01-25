1c352281e40c67d1404d2b87e7fb011a3034f7d6
Lukas Hu <lukashu22@gmail.com>
2025-01-24 23:49:29 -0800

    Added components

---
+++ src/components/userInput.jsx
@@ -0,0 +1,16 @@
+const UserInputArea = () => {
+    return (
+        <form className='add-form'>
+            <div className='form-control'>
+                <label>test label</label>
+                <input type='text' placeholder='UserInputArea'/>
+            </div>
+            <div className='form-control'>
+                <label>test label</label>
+                <input type='text' placeholder='UserInputArea'/>
+            </div>
+        </form>
+    )
+}
+
+export default UserInputArea
