e2c0d010cd196a9cfc4b1ac4f140253f6224459e
Kailee Kaocharoen <kkaochar@uci.edu>
2025-01-25 15:03:39 -0800

    scroll now on travel plans

--- src/pages/TravelPlans.jsx
+++ src/pages/TravelPlans.jsx
@@ -4,13 +4,15 @@ import logo from "../assets/wizard.gif";
 const TravelPlans = () => {
     return (
       <div id="homepage">
+        <div id="scroll_text">
+            weather information
+        </div>
         <div id="wizard_chatbox_bottom"> 
             <p>
                 with a little bit of magic, <br></br>
                 here's what i can suggest for your trip! 
-            </p>
-            
-            </div>
+            </p>    
+        </div>
         <img id="travel_plan_wizard" src={logo} alt="loading..." />
       </div>
     );
