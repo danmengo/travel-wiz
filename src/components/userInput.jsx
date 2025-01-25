ae761b7138777b7467cd5e91073ed150971baa0e
Daniel <daneilmeng530@gmail.com>
2025-01-25 10:51:55 -0800

    added gif

--- src/components/userInput.jsx
+++ src/components/userInput.jsx
@@ -1,31 +1,32 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const UserInputArea = () => {
-    const [position, setPosition] = useState({top: 100, left: 100});
-    // const moveInput = () => {
-    //     setPosition({
-    //         top: position.top + 20,
-    //         left: position.left + 20,
-    //     });
-    // }
-    return (
-        <form className='add-form'>
-            <div className='form-control'>
-                <label>Username</label>
-                <input type='text' placeholder='Enter Username'/>
-            </div>
-            <div className='form-control'>
-                <label>Password</label>
-                <input type='text' placeholder='Enter Password'/>
-            </div>
-            <input type="submit" 
-            value="Login" 
-            // onClick={moveInput} 
-            className="btn" 
-            // style={{position: "absolute", top: `${position.top}px`, left: `${position.left}px`,}}
-            />
-        </form>
-    )
-}
+  const [position, setPosition] = useState({ top: 100, left: 100 });
+  // const moveInput = () => {
+  //     setPosition({
+  //         top: position.top + 20,
+  //         left: position.left + 20,
+  //     });
+  // }
+  return (
+    <form className="add-form">
+      <div className="form-control">
+        <label>Username</label>
+        <input type="text" placeholder="Enter Username" />
+      </div>
+      <div className="form-control">
+        <label>Password</label>
+        <input type="text" placeholder="Enter Password" />
+      </div>
+      <input
+        type="submit"
+        value="Login"
+        // onClick={moveInput}
+        className="btn"
+        // style={{position: "absolute", top: `${position.top}px`, left: `${position.left}px`,}}
+      />
+    </form>
+  );
+};
 
-export default UserInputArea
+export default UserInputArea;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,9 @@
 import UserInputArea from "../components/userInput";
-
+import logo from "../assets/wizard.gif";
 const Homepage = () => {
   return (
     <div>
+      <img src={logo} alt="loading..." width="150" height="150" />
       Homepage
       <UserInputArea />
     </div>
