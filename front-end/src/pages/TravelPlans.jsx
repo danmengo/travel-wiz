72b3023b5ee3c596d7c6b3d4e0910aaf48665d06
Daniel <daneilmeng530@gmail.com>
2025-01-25 21:59:30 -0800

    stored local variable

--- front-end/src/pages/TravelPlans.jsx
+++ front-end/src/pages/TravelPlans.jsx
@@ -3,23 +3,23 @@ import logo from "../assets/wizard.gif";
 import InteractiveMap from "../components/InteractiveMap";
 
 const TravelPlans = () => {
-    return (
-      <div id="travel_plan_page" >
-
-        <div id="scroll_text">
-            weather information
-            <InteractiveMap />
-        </div>
-        
-        <div id="wizard_chatbox_bottom"> 
-            <p>
-                with a little bit of magic, <br></br>
-                here's what i can suggest for your trip! 
-            </p>    
-        </div>
-        
-        <img id="travel_plan_wizard" src={logo} alt="loading..." />
+  const userInput = localStorage.getItem("userInput");
+  return (
+    <div id="travel_plan_page">
+      <div id="scroll_text">
+        weather information
+        <InteractiveMap />
       </div>
-    );
-  };
-  export default TravelPlans;
+
+      <div id="wizard_chatbox_bottom">
+        <p>
+          with a little bit of magic, <br></br>
+          here's what i can suggest for your trip to {userInput}!
+        </p>
+      </div>
+
+      <img id="travel_plan_wizard" src={logo} alt="loading..." />
+    </div>
+  );
+};
+export default TravelPlans;
