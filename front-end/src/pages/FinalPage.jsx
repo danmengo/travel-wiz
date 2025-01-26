7a69671457fc2eaabf1242d85d29e872271d840b
Kailee Kaocharoen <kkaochar@uci.edu>
2025-01-26 08:19:58 -0800

    gemini recomendation style fixed

--- front-end/src/pages/FinalPage.jsx
+++ front-end/src/pages/FinalPage.jsx
@@ -8,7 +8,6 @@ const FinalPage = () => {
     <div id = "finalpage">
       <div id="wizard_chatbox"> With my magic...<br></br>here are some travel suggestions! </div>
       <img id="wizard" src={logo} alt="loading..." />
-      <div id="scroll_results"> data data data yay go team </div>
       <div id="scroll_results"><GeminiRecommendations /></div>
       <BackButton destination="/destination"/>
     </div>
