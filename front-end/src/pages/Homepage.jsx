8316cc461adeba6a91513f09ff6fa7f680b490cf
Lukas Hu <lukashu22@gmail.com>
2025-01-25 20:42:54 -0800

    Added input to the home page

--- front-end/src/pages/Homepage.jsx
+++ front-end/src/pages/Homepage.jsx
@@ -1,14 +1,13 @@
 import "./Homepage.css";
 import logo from "../assets/wizard.gif";
-// import UserInputArea from "../components/userInput";
+import UserInputArea from "../components/userInput";
 
 const Homepage = () => {
   return (
-    <div>
+    <div id = "homepage">
       <div id="wizard_chatbox"> Welcome traveler! I'm the travel wizard! Where would you like to go? </div>
       <img id="wizard" src={logo} alt="loading..." />
-      {/* <UserInputArea /> */}
-      <div id="weather_display"> display weather here </div>
+      <div id="weather_display"><UserInputArea /></div>
     </div>
   );
 };
--- front-end/src/pages/TravelPlans.jsx
+++ front-end/src/pages/TravelPlans.jsx
@@ -1,11 +1,8 @@
 import "./TravelPlans.css";
 import logo from "../assets/wizard.gif";
 import InteractiveMap from "../components/InteractiveMap";
-<<<<<<< HEAD
-=======
 import wiz_scoll from "../assets/wizard_scroll.png";
 import UserInputArea from "../components/userInput";
->>>>>>> 6bdbb4fa139645c2f59dcb5f971006d1945c3a66
 
 const TravelPlans = () => {
     return (
