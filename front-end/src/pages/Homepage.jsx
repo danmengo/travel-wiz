09a1550c2b88d32482485cce5fd26ca35fb65978
Lukas Hu <lukashu22@gmail.com>
2025-01-25 17:27:27 -0800

    Deleted unecessary files

--- front-end/src/pages/Homepage.jsx
+++ front-end/src/pages/Homepage.jsx
@@ -7,10 +7,6 @@ const Homepage = () => {
     <div id="homepage">
       <div id="wizard_chatbox"> Welcome traveler! I'm the travel wizard! </div>
       <img id="wizard" src={logo} alt="loading..." />
-<<<<<<< HEAD:src/pages/Homepage.jsx
-=======
-      
->>>>>>> main:front-end/src/pages/Homepage.jsx
       <UserInputArea />
       
       <div id="weather_display"> display weather here </div>
